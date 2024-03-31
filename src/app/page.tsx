654fc310d36c17dead69da49d166795cf4d1633c
burunyuu23 <ezzfvkoko@gmail.com>
2024-03-31 09:19:46 +0300

    add welcomes info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import styles from "./page.module.css";
 export default function Home() {
     return (
         <main className={styles.main}>
-            Ratingus
+            <h3>Добро пожаловать в</h3>
+            <h1>Ratingus</h1>
         </main>
     );
 }
