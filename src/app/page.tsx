2dd594c941e68c62b991f2aac00a0d73ff613bc3
burunyuu23 <ezzfvkoko@gmail.com>
2024-03-31 09:32:40 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
         <main className={styles.main}>
             <h3>Добро пожаловать в</h3>
             <h1>Ratingus</h1>
+            <div>Всем привееет!</div>
         </main>
     );
 }
