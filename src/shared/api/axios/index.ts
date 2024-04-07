da18c8b67536026efd90bd65995afe49df6b75a5
burunyuu23 <ezzfvkoko@gmail.com>
2024-04-07 15:42:53 +0300

    change axios baseURL

--- src/shared/api/axios/index.ts
+++ src/shared/api/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NEXTAUTH_BACKEND_URL,
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
