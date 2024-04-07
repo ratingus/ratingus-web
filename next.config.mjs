097cb64c1ee9dbc60c7d59e72204a9a03f779d1d
burunyuu23 <ezzfvkoko@gmail.com>
2024-04-07 15:39:26 +0300

    config for msw

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            if (Array.isArray(config.resolve.alias)) {
+                config.resolve.alias.push({ name: "msw/browser", alias: false })
+            } else {
+                config.resolve.alias["msw/browser"] = false
+            }
+        } else {
+            if (Array.isArray(config.resolve.alias)) {
+                config.resolve.alias.push({ name: "msw/node", alias: false })
+            } else {
+                config.resolve.alias["msw/node"] = false
+            }
+        }
+        return config;
+    },
+};
 
 export default nextConfig;
