696b5a21bac755d7683de4584e4b0424b50d7afa
burunyuu23 <ezzfvkoko@gmail.com>
2024-04-20 18:37:07 +0300

    add Label

---
+++ src/shared/components/Label/Label.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Label } from "./Label";
+
+const meta: Meta<typeof Label> = {
+  component: Label,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Label>;
+
+export const Primary: Story = {
+  args: {
+    children: "Это текст",
+  },
+};
