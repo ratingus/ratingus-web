c79790172bfe44ec973c5d287f344d570f84a898
burunyuu23 <ezzfvkoko@gmail.com>
2024-04-20 20:23:20 +0300

    change font

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,8 +1,16 @@
 import type { Preview } from "@storybook/react";
+import { Roboto } from "next/font/google";
+
 import "@/app/(router)/(main)/globals.scss";
 
 // @ts-ignore
 import vars from "@/shared/styles/vars.module.scss";
+
+const font = Roboto({
+  weight: ["300", "500"],
+  variable: "--font-main",
+  subsets: ["cyrillic", "latin"],
+});
 
 const preview: Preview = {
   parameters: {
@@ -22,6 +30,7 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [(Story) => <div className={font.className}>{Story()}</div>],
 };
 
 export default preview;
--- src/app/(router)/(main)/layout.tsx
+++ src/app/(router)/(main)/layout.tsx
@@ -1,12 +1,16 @@
 import { ReactNode } from "react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 
 import "./globals.scss";
 
 import { GlobalProvider } from "@/app/_providers";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Roboto({
+  weight: ["300", "500"],
+  variable: "--font-main",
+  subsets: ["cyrillic", "latin"],
+});
 
 export const metadata: Metadata = {
   title: "Рейтингус",
@@ -21,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body className={inter.className}>
+      <body className={font.className}>
         <GlobalProvider>{children}</GlobalProvider>
       </body>
     </html>
