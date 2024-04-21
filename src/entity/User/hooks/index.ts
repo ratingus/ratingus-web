7307d63fa44dfb35d488e8bd7f84485972dabd32
burunyuu23 <ezzfvkoko@gmail.com>
2024-04-21 23:25:42 +0300

    add User school and class ids

--- src/entity/User/hooks/index.ts
+++ src/entity/User/hooks/index.ts
@@ -1,15 +1,16 @@
 import { getFioByUser } from "@/entity/User/helpers";
-import { User } from "@/entity/User/model";
+import { User, UserDetails } from "@/entity/User/model";
 
 export const useUser = (): User => {
   // TODO: обработка jwt
-  const user = {
+  const user: UserDetails = {
     id: 1,
     login: "Логин",
     name: "Имя",
     surname: "Фамилия",
     patronymic: "Отчество",
-    fio: "",
+    schoolId: 1,
+    classId: 1,
     birthdate: new Date(Date.UTC(2002, 2, 12)),
   };
   return {
