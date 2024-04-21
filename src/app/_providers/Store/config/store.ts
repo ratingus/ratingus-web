348d0026665060290fea402a7b0b48812615d422
burunyuu23 <ezzfvkoko@gmail.com>
2024-04-21 23:28:13 +0300

    add api and mocking for announcements

--- src/app/_providers/Store/config/store.ts
+++ src/app/_providers/Store/config/store.ts
@@ -1,14 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import { announcementsApi } from "@/entity/Announcement/query";
+import { announcementSlice } from "@/entity/Announcement/store";
+import { schoolSlice } from "@/entity/School/store";
 import { modalSlice } from "@/shared/components/Modal/slice";
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       [modalSlice.name]: modalSlice.reducer,
+      [announcementSlice.name]: announcementSlice.reducer,
+      [schoolSlice.name]: schoolSlice.reducer,
+      [announcementsApi.reducerPath]: announcementsApi.reducer,
     },
     middleware: (getDefaultMiddleware) => {
-      return getDefaultMiddleware({});
+      return getDefaultMiddleware({}).concat(announcementsApi.middleware);
     },
   });
 };
