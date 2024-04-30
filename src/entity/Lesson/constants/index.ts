2b942328e1efb2d9a93b976b78c0b721af60c882
burunyuu23 <ezzfvkoko@gmail.com>
2024-04-30 12:43:33 +0300

    add Lesson and Study models

---
+++ src/entity/Lesson/constants/index.ts
@@ -0,0 +1,5 @@
+export const LESSONS_CONTEXT_PATH = "/diary";
+
+export const LESSONS_PATH = `${LESSONS_CONTEXT_PATH}`;
+export const LESSONS_BY_WEEK = (week: number) =>
+  `${LESSONS_CONTEXT_PATH}?week=${week}`;
