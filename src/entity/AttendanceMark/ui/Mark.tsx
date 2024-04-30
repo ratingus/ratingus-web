72ba8bfd2708286baa278680681434ce367ce3a9
burunyuu23 <ezzfvkoko@gmail.com>
2024-04-30 18:08:24 +0300

    add variants for Mark

--- src/entity/AttendanceMark/ui/Mark.tsx
+++ src/entity/AttendanceMark/ui/Mark.tsx
@@ -3,21 +3,25 @@ import React from "react";
 import { ATTENDANCE } from "@/entity/AttendanceMark/constants";
 import { getColorByMark } from "@/entity/AttendanceMark/helpers";
 import { Lesson } from "@/entity/Lesson/model";
-import { Typography } from "@/shared/components/Typography/Typography";
+import {
+  Typography,
+  TypographyVariant,
+} from "@/shared/components/Typography/Typography";
 
 // import styles from './Mark.module.scss';
 
 type MarkProps = {
   mark?: Lesson["mark"];
   attendance?: Lesson["attendance"];
+  variant?: TypographyVariant;
 };
 
-const Mark = ({ mark, attendance }: MarkProps) => {
+const Mark = ({ mark, attendance, variant = "h2" }: MarkProps) => {
   if (!mark && !attendance) {
     return null;
   }
   return (
-    <Typography variant="h2" color={getColorByMark(mark || attendance)}>
+    <Typography variant={variant} color={getColorByMark(mark || attendance)}>
       {mark ||
         (attendance ? ATTENDANCE[attendance].slice(0, 1).toUpperCase() : "")}
     </Typography>
