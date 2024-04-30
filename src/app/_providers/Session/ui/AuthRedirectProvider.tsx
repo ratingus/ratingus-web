7a44bbd7724d56eed2b170a54dc3dd9fdc9c98b4
burunyuu23 <ezzfvkoko@gmail.com>
2024-04-30 21:04:07 +0300

    change provider

--- src/app/_providers/Session/ui/AuthRedirectProvider.tsx
+++ src/app/_providers/Session/ui/AuthRedirectProvider.tsx
@@ -1,7 +1,9 @@
 "use client";
-import { ReactNode, useEffect } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
+import HeaderIcon from "@icons/header.svg";
+import { motion } from "framer-motion";
 import Cookies from "js-cookie";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 type AuthRedirectProviderProps = {
   children: ReactNode;
@@ -9,17 +11,37 @@ type AuthRedirectProviderProps = {
 
 const AuthRedirectProvider = ({ children }: AuthRedirectProviderProps) => {
   const router = useRouter();
+  const [isAuthRedirected, setIsAuthRedirected] = useState<boolean>();
 
   useEffect(() => {
-    // Проверяем, что код выполняется на клиенте
     if (typeof window !== "undefined") {
       const isUserLoggedIn = Cookies.get("isLogged") === "true";
 
       if (!isUserLoggedIn) {
         router.push("/login");
       }
+      setIsAuthRedirected(true);
     }
-  }, []);
+  }, [router]);
+
+  // TODO: вынести в loading.tsx и пока не загрузилось, вызывать лоадер некста, а не вот это вот что это вообще такое
+  if (!isAuthRedirected) {
+    return (
+      <motion.div
+        style={{
+          height: "100dvh",
+          width: "100dvw",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        animate={{ opacity: [0.5, 1, 0.5] }}
+        transition={{ duration: 1.5, repeat: Infinity }} // Adjust the duration as needed
+      >
+        <HeaderIcon />
+      </motion.div>
+    );
+  }
 
   return <>{children}</>;
 };
