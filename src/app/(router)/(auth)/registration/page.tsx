ac3d70b66e0362e5a9384c98d9012a3a321ae1f3
burunyuu23 <ezzfvkoko@gmail.com>
2024-06-18 00:05:28 +0300

    фикс формы регистрации + добавил чекбокс с пользовательским соглашением

--- src/app/(router)/(auth)/registration/page.tsx
+++ src/app/(router)/(auth)/registration/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FormEventHandler, useRef } from "react";
+import React, { FormEventHandler, useRef } from "react";
 import { toast } from "react-toastify";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -10,6 +10,7 @@ import styles from "./page.module.scss";
 import Avatar from "@/entity/User/ui/Avatar";
 import api from "@/shared/api/axios";
 import Button from "@/shared/components/Button/Button";
+import { Checkbox } from "@/shared/components/Checkbox";
 import { Input } from "@/shared/components/Input/Input";
 import { Typography } from "@/shared/components/Typography/Typography";
 
@@ -25,18 +26,41 @@ export default function Registration() {
     e.preventDefault();
     if (form.current) {
       const formData = new FormData(form.current);
+
+      const login = formData.get("login")?.toString() || "";
+      const password = formData.get("pass")?.toString() || "";
+      const name = formData.get("name")?.toString() || "";
+      const surname = formData.get("surname")?.toString() || "";
+      const patronymic = formData.get("patronymic")?.toString() || "";
+      const birthDate = formData.get("birthDate")?.toString();
+      const isLegalRulesAccepted = Boolean(
+        formData.get("isLegalRulesAccepted")?.toString() || "",
+      );
+
+      if (!login || !password || !name || !surname || !birthDate) {
+        toast("Не все поля заполнены!", {
+          type: "error",
+        });
+        return;
+      }
+      if (!isLegalRulesAccepted) {
+        toast('Согласитесь с "Пользовательским соглашением"', {
+          type: "error",
+        });
+        return;
+      }
       try {
         await api.post("/auth/register", {
-          name: formData.get("name"),
-          surname: formData.get("surname"),
-          patronymic: formData.get("patronymic"),
-          birthDate: formData.get("birthDate"),
-          login: formData.get("login"),
-          password: formData.get("pass"),
+          name,
+          surname,
+          patronymic,
+          birthDate: new Date(birthDate),
+          login,
+          password,
         });
         const response = await signIn("credentials", {
-          login: formData.get("login"),
-          password: formData.get("pass"),
+          login,
+          password,
           redirect: false,
         });
         if (!response) throw new Error("");
@@ -67,11 +91,13 @@ export default function Registration() {
           <Avatar size={64} />
           <div className={styles.baseInputs}>
             <Input
+              form="register"
               name="login"
               className={styles.baseInput}
               placeholder="Логин"
             />
             <Input
+              form="register"
               name="pass"
               className={styles.baseInput}
               placeholder="Пароль"
@@ -80,12 +106,27 @@ export default function Registration() {
           </div>
         </div>
         <div className={styles.miniForm}>
-          <Input name="surname" placeholder="Фамилия" />
-          <Input name="name" placeholder="Имя" />
+          <Input form="register" name="surname" placeholder="Фамилия" />
+          <Input form="register" name="name" placeholder="Имя" />
         </div>
         <div className={styles.miniForm}>
-          <Input name="patronymic" placeholder="Отчество" />
-          <Input name="birthDate" placeholder="Дата рождения" type="date" />
+          <Input form="register" name="patronymic" placeholder="Отчество" />
+          <Input
+            form="register"
+            name="birthDate"
+            placeholder="Дата рождения"
+            type="date"
+            max="2024-01-01"
+          />
+        </div>
+        <div className={styles.miniForm}>
+          <Checkbox form="register" name="isLegalRulesAccepted" />
+          <Typography className={styles.miniFormText} variant="body">
+            Я согласен с{" "}
+            <Link href="/privacy.pdf" target="_blank">
+              “Пользовательским соглашением”
+            </Link>
+          </Typography>
         </div>
         <Button className={styles.button} variant="important">
           Регистрация
