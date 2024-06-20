322200a1e2c294d8239a9a9166e21efadc300433
dnlkk <34377854+burunyuu23@users.noreply.github.com>
2024-06-20 13:01:20 +0300

    RATINGUS-219 Для мобилок сделать thumb для скачивания apk, чтобы на мобилке нельзя было пользоваться сайтом (#43)
    * useResolution
    
    * запретил

--- src/app/_providers/Session/ui/AuthRedirectProvider.tsx
+++ src/app/_providers/Session/ui/AuthRedirectProvider.tsx
@@ -2,17 +2,23 @@
 import React, { ReactNode, useEffect } from "react";
 import HeaderIcon from "@icons/header.svg";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 
 import { actionSetSelectedSchool } from "@/entity/School/store";
+import Button from "@/shared/components/Button/Button";
+import { Typography } from "@/shared/components/Typography/Typography";
 import { useAppDispatch } from "@/shared/hooks/rtk";
+import useResolution from "@/shared/hooks/useResolution";
 
 type AuthRedirectProviderProps = {
   children: ReactNode;
 };
 
 const AuthRedirectProvider = ({ children }: AuthRedirectProviderProps) => {
+  const { isMobile } = useResolution();
   const router = useRouter();
   const path = usePathname();
   const dispatch = useAppDispatch();
@@ -32,6 +38,48 @@ const AuthRedirectProvider = ({ children }: AuthRedirectProviderProps) => {
       }
     }
   }, [router, path, status]);
+
+  if (isMobile) {
+    return (
+      <div
+        style={{
+          height: "100dvh",
+          width: "100dvw",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+          alignItems: "center",
+          gap: 16,
+        }}
+      >
+        <Typography
+          variant="h4"
+          style={{
+            textAlign: "center",
+          }}
+        >
+          На сайте пока не поддерживаются мобильные устройства
+        </Typography>
+        <Image
+          src="/images/mobile_ratingus.png"
+          alt="Мобильный рейтингус"
+          height={160}
+          width={300}
+          style={{
+            textAlign: "center",
+            objectFit: "cover",
+            width: "300px",
+            height: "160px",
+            objectPosition: "350% 0",
+          }}
+        />
+        <Link href="https://github.com/ratingus/ratingus_mobile/releases/latest/download/ratingus.apk">
+          <Button variant="important">Скачайте мобильное приложение!</Button>
+        </Link>
+      </div>
+    );
+  }
 
   // TODO: вынести в loading.tsx и пока не загрузилось, вызывать лоадер некста, а не вот это вот что это вообще такое
   if (status === "loading") {
